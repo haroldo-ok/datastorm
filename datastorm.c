d5b8134cbb92ebd5e9aa7ecb8f2d1bd445842d18
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-02-16 22:53:54 -0200

    Drawing the lanes

--- datastorm.c
+++ datastorm.c
@@ -5,6 +5,14 @@
 #include "SMSlib/src/SMSlib.h"
 #include "gfx.h"
 
+#define LANE_COUNT 7
+#define LANE_CHAR_HEIGHT 3
+#define LANE_TOP 1
+#define LANE_BOTTOM (LANE_TOP + LANE_COUNT * LANE_CHAR_HEIGHT)
+#define LANE_PIXEL_TOP_LIMIT (LANE_TOP * 8 + 8)
+#define LANE_PIXEL_HEIGHT (LANE_CHAR_HEIGHT * 8)
+#define LANE_BASE_TILE 0xB0
+
 #define PF_OFFSET_X 0
 #define PF_OFFSET_Y 0
 
@@ -602,6 +610,56 @@ void load_font (void) {
 	}
 }
 
+void draw_lanes() {
+  unsigned char i;
+  unsigned int *p, *p2;
+  unsigned int y;
+  unsigned int buffer[32];
+
+  p = buffer;
+  p2 = p + 31;
+
+  // Creates the gradient for the lanes (yes, I'm STILL too lazy to make a map.)
+
+  for (i = 0; i != 5; i++) {
+    *p = LANE_BASE_TILE;
+    *p2 = LANE_BASE_TILE;
+    p++; p2--;
+  }
+
+  for (i = 4; i != 9; i++) {
+    *p = LANE_BASE_TILE + 1;
+    *p2 = LANE_BASE_TILE + 1;
+    p++; p2--;
+  }
+
+  for (i = 9; i != 16; i++) {
+    *p = LANE_BASE_TILE + 2;
+    *p2 = LANE_BASE_TILE + 2;
+    p++; p2--;
+  }
+
+  // Draws the topmost/bottommost dividers
+
+  SMS_loadTileMap(0, LANE_TOP, buffer, 64);
+  SMS_loadTileMap(0, LANE_BOTTOM, buffer, 64);
+
+  // Opens a hole in the middle
+
+  buffer[14] = LANE_BASE_TILE + 3;
+  buffer[15] = 0;
+  buffer[16] = 0;
+  buffer[17] = LANE_BASE_TILE + 4;
+
+  // Draws remaining dividers
+
+  y = LANE_TOP + LANE_CHAR_HEIGHT;
+  for (i = 2; i != (LANE_COUNT + 1); i++) {
+    SMS_loadTileMap(0, y, buffer, 64);
+    y += LANE_CHAR_HEIGHT;
+  }
+}
+
 void main(void) {
   unsigned char i;
 
@@ -610,8 +668,15 @@ void main(void) {
   SMS_loadBGPalette(ship_pal);
   SMS_loadSpritePalette(ship_pal);
 
-  title_screen();
-  simple_rl();
+  draw_lanes();
+
+  SMS_displayOn();
+
+  while (true) {
+    kp = SMS_getKeysPressed();
+
+    SMS_waitForVBlank();
+  }
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
