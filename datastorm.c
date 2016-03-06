d77199d6197ce395e4ccb78a3acef4e2356caec4
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2016-03-06 17:57:44 -0300

    Increased version to 1.0

--- datastorm.c
+++ datastorm.c
@@ -933,6 +933,6 @@ void main(void) {
 }
 
 SMS_EMBED_SEGA_ROM_HEADER(9999,0); // code 9999 hopefully free, here this means 'homebrew'
-SMS_EMBED_SDSC_HEADER(0,5, 2016,2,28, "Haroldo-OK\\2016", "Data Storm",
+SMS_EMBED_SDSC_HEADER(1,0, 2016,3,6, "Haroldo-OK\\2016", "Data Storm",
   "A fast paced shoot-em-up.\n"
   "Built using devkitSMS & SMSlib - https://github.com/sverx/devkitSMS");
